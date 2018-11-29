088321abf22545f4852462ddfbbbcc858e754a2a
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-11-29 19:18:00 +0100

    Add post edit option

--- client/modules/Post/PostReducer.js
+++ client/modules/Post/PostReducer.js
@@ -1,23 +1,31 @@
-import { ADD_POST, ADD_POSTS, DELETE_POST } from './PostActions';
+import { ADD_POST, ADD_POSTS, DELETE_POST, EDIT_POST } from "./PostActions";
 
-// Initial State
 const initialState = { data: [] };
 
 const PostReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_POST :
+    case ADD_POST:
       return {
-        data: [action.post, ...state.data],
+        data: [action.post, ...state.data]
       };
 
-    case ADD_POSTS :
+    case ADD_POSTS:
       return {
-        data: action.posts,
+        data: action.posts
       };
 
-    case DELETE_POST :
+    case DELETE_POST:
       return {
-        data: state.data.filter(post => post.cuid !== action.cuid),
+        data: state.data.filter(post => post.cuid !== action.cuid)
+      };
+
+    case EDIT_POST:
+      return {
+        data: state.data.map(post => {
+          return post.cuid === action.cuid
+            ? Object.assign({}, post, action.post)
+            : post;
+        })
       };
 
     default:
@@ -25,13 +33,9 @@ const PostReducer = (state = initialState, action) => {
   }
 };
 
-/* Selectors */
-
-// Get all posts
 export const getPosts = state => state.posts.data;
 
-// Get post by cuid
-export const getPost = (state, cuid) => state.posts.data.filter(post => post.cuid === cuid)[0];
+export const getPost = (state, cuid) =>
+  state.posts.data.filter(post => post.cuid === cuid)[0];
 
-// Export Reducer
 export default PostReducer;
